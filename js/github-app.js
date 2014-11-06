b6a99e6b8f98834b66c43c2a2ba4ce694543c807
Linnea <linnea_watson@hotmail.com>
2014-11-06 12:18:24 -0800

    Done

--- js/github-app.js
+++ js/github-app.js
@@ -8,11 +8,27 @@
 
 
 angular.module('GitHubApp', [])
-    .controller('GitHubController', function($scope) {
+    .controller('GitHubController', function($scope, $http) { //$http allows us to make an http request
         $scope.userName = 'drstearns';
         $scope.getRepos = function() {
 
-            //add code here
+            $scope.loading = true;
+            $http.get('https://api.github.com/users/' + $scope.userName + '/repos')
+                .success(function(data) {
+                    $scope.repos = data;
+                    $scope.errorMessage = null;
+                //.success is a function on returned object, takes parameter that's called when request is finished
+                })
 
+                .error(function(err) {
+                   //alert(err.message);
+                    $scope.errorMessage = err.message;
+
+                })
+                .finally(function() {
+                    $scope.loading = false;
+                });
+
+            // when we make an http request, it doesnt block our code
         };
     });
